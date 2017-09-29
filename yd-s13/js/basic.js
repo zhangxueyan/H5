9c0ac5b902b52d65f77b698ccac943b09d050893
zhangxueyan <1632895066@qq.com>
2017-09-30 07:46:57 +0800

    'update'

--- yd-s13/js/basic.js
+++ yd-s13/js/basic.js
@@ -1,33 +1,33 @@
 Zepto(function($){
-  var $month = $(".month");
-  var $date = $(".date");
-  var date = new Date();
-  var getDay = date.getDay();
-  var getDate = date.getDate();
-  var getMonth = date.getMonth();
-  switch(getDay){
-   case 0:
-   getDate+=3
-   break;
-   case 1:
-   getDate+=2
-   break;
-   case 2:
-   getDate+=1
-   break;
-   case 3:
-   getDate+=2
-   break;
-   case 4:
-   getDate+=1
-   break;
-   case 5:
-   getDate+=2
-   break;
-   case 6:
-   getDate+=1
-   break;
-  }
-  $month.text(getMonth+1)
-  $date.text(getDate)
+  // 倒计时效果
+      var $month = $(".month");
+      var $date = $(".date");
+      var myDate = new Date();//鏈堟棩骞�
+      var date = myDate.getDate();//鍑犲彿
+      var day = myDate.getDay();//鍛ㄥ嚑 濡傛灉鏄懆7  date+7
+      switch(day){
+       case 0:
+       myDate.setDate(date+3)
+       break;
+       case 1:
+       myDate.setDate(date+2)
+       break;
+       case 2:
+       myDate.setDate(date+1)
+       break;
+       case 3:
+       myDate.setDate(date+2)
+       break;
+       case 4:
+       myDate.setDate(date+1)
+       break;
+       case 5:
+       myDate.setDate(date+2)
+       break;
+       case 6:
+       myDate.setDate(date+1)
+       break;
+      }
+     $month.text(myDate.getMonth()+1)
+     $date.text(myDate.getDate())
 })
