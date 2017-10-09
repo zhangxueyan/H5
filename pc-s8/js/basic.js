7009d0ea4d47ad81bb2c7e889938b6441c836fb0
zhangxueyan <1632895066@qq.com>
2017-10-09 16:38:00 +0800

    'update'

--- pc-s8/js/basic.js
+++ pc-s8/js/basic.js
@@ -22,20 +22,29 @@
       }
       $month.text(myDate.getMonth()+1);
       $date.text(myDate.getDate());
+      //banner 交互效果
+      var $ban_btn = $(".ban-dl").find("a");
+      setTimeout(function(){
+        $ban_btn.removeClass("animated bounceInLeft s-step2 bounceInRight s-step3")
+      },2000)
+      $('#element').toggleStyle($ban_btn,"animated pulse")
       //s1 交互效果
       var $s1_li = $(".s1-li");
-      $s1_li.hover(function(){
-        var $item = $(this);
-        $item.toggleClass("animated bounce")
-      })
+      $('#element').toggleStyle($s1_li,"animated bounce")
+      //s3 交互效果
+      var $s3_li = $(".s3-li");
+      $('#element').toggleStyle($s3_li,"animated bounce")
+      //s4 交互效果
+      var $s4_li = $(".s4-li");
+      $('#element').toggleStyle($s4_li,"animated pulse")
       // s5交互效果
       var $s5_li = $(".s5-li");
-      $s5_li.hover(function(){
-        var $item = $(this);
-        $item.toggleClass("hover animated swing")
-      })
+      $('#element').toggleStyle($s5_li,"hover animated swing")
+      // s7交互效果
+      var $s7_li = $(".s7-li");
+      $('#element').toggleStyle($s7_li,"animated pulse")
       //s6滚动信息
-        var speed=50;//控制速度
+        var speed=30;//控制速度
         var tab=document.getElementById("demo");
         var tab1=document.getElementById("demo1");
         var tab2=document.getElementById("demo2");
