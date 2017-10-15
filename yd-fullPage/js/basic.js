92c05e975527382b6944e40e24c96931dc83af98
zhangxueyan <1632895066@qq.com>
2017-10-15 20:27:09 +0800

    'update'

---
+++ yd-fullPage/js/basic.js
@@ -0,0 +1,8 @@
+Zepto(function($){
+     var mySwiper = new Swiper ('.swiper-container', {
+        direction : 'vertical',
+        loop: true,
+        pagination: '.swiper-pagination',
+        autoplay:3500
+     })    
+})
---
+++ yd-s18/js/basic.js
@@ -0,0 +1,33 @@
+Zepto(function($){
+  // 倒计时效果
+      var $month = $(".month");
+      var $date = $(".date");
+      var myDate = new Date();//鏈堟棩骞�
+      var date = myDate.getDate();//鍑犲彿
+      var day = myDate.getDay();//鍛ㄥ嚑 濡傛灉鏄懆7  date+7
+      switch(day){
+       case 0:
+       myDate.setDate(date+3)
+       break;
+       case 1:
+       myDate.setDate(date+2)
+       break;
+       case 2:
+       myDate.setDate(date+1)
+       break;
+       case 3:
+       myDate.setDate(date+2)
+       break;
+       case 4:
+       myDate.setDate(date+1)
+       break;
+       case 5:
+       myDate.setDate(date+2)
+       break;
+       case 6:
+       myDate.setDate(date+1)
+       break;
+      }
+     $month.text(myDate.getMonth()+1)
+     $date.text(myDate.getDate())
+})
