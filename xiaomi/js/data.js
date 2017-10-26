29383a708921d27a1ef6e6e9b0369fa5655ebe6d
zhangxueyan <1632895066@qq.com>
2017-10-26 18:15:21 +0800

    'uddate'

---
+++ xiaomi/js/data.js
@@ -0,0 +1,20 @@
+window.onload=function(){
+     var vm = new Vue({
+        el: '#app',
+        data: {
+           message: 'Hello',
+           obj:''
+        },
+       created:function(){
+         var url="https://zhangxueyan.github.io/private/xiaomi/data.json";
+         $.ajax({
+           url:url,
+           dataType:"json",
+           success:function(result){
+            console.log(this)
+            this.obj = result
+           }
+          });
+        }
+      })
+}
