b703974b7c987449ade7263a04b467c5599bab70
zhangxueyan <1632895066@qq.com>
2017-12-01 18:44:11 +0800

    'update'

--- pc-s12/js/basic.js
+++ pc-s12/js/basic.js
@@ -10,52 +10,20 @@
     var $slideDown = $item.find(".slideDown")
     $slideDown.fadeOut();
   })
-   // left right-slide 
-    var $close= $(".suspend-close") 
-    $close.click(function(){
-      var $item = $(this);
-      $item.parent(".suspend").hide()
+  //banner
+    var mySwiper1 = new Swiper ('.swiper-container1', {
+       loop: true,
+       initialSlide :1,
+       pagination: '.swiper-pagination',
+       paginationClickable :true
+       // autoplay:3000
     })
-  //pop-tc
-    var $popup1 = $('.popup1');
-    var $popup2 = $('.popup2');
-    var $popup3 = $('.popup3');
-    $('#element').showPopup($popup1,3000);  
-    $(".popup-close1").click(function(){
-      $('#element').hidePopup($popup1);
-      $('#element').showPopup($popup2,10000);
-    })
-    $(".popup-close2").click(function(){
-      $('#element').hidePopup($popup2);
-      $('#element').showPopup($popup3,10000);
-    })
-    $(".popup-close3").click(function(){
-      $('#element').hidePopup($popup3);
-    })
-    var $popup_ul = $(".popup").find("ul");
-    $popup_ul.click(function(){
-      $(".popup").fadeOut()
-    })
-  // s0
-  var $s0_item = $(".s0-item");
-  $s0_item.mouseenter(function(){
-    var $item = $(this);
-    $s0_item.removeClass("active");
-    $item.addClass("active")
-  })
-   // s1
-   var $s1_switch = $(".s1-switch");
-   $s1_switch.hover(function(){
-    $(this).toggleClass("active")
-   })
-   // s3
-   var $s3_li = $(".s3-li");
-   $('#element').toggleStyle($s3_li,"animated bounce");
-   // s6
-   var $s6_li = $(".s6-li");
-   $s6_li.hover(function(){
-     $(this).toggleClass("active")
-   })
+    // $(".swiper-container1").mouseenter(function(){
+    //   mySwiper1.stopAutoplay()
+    // }).mouseleave(function(){
+    //   mySwiper1.startAutoplay()
+    // })
+
    // Deadline for registration
     var $month = $(".month");
     var $date = $(".date");
@@ -78,13 +46,15 @@
       }else if(day == 6){
         myDate.setDate(date+1);
       }
+      var newMonth = myDate.getMonth()+1;
+      var newDate = myDate.getDate();
+      $month.text(newMonth<10?'0'+newMonth:newMonth);
+      $date.text(newDate<10?'0'+newDate:newDate);
       $year.text(myDate.getFullYear());
-      $month.text(myDate.getMonth()+1);
-      $date.text(myDate.getDate());
 
       //底部bot-fixed month
+      var $botYear = $(".bot-year");
       var $botMonth = $(".bot-month");
-      var $botYear = $(".bot-year")
       var botDate = new Date();// 12/31
       if(date>27){
         botDate.setDate(botDate.getDate()+5)
